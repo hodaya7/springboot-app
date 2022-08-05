7886c30bd1f52f505278ec3d38e3142be2c992f5
hodaya7 <109215336+hodaya7@users.noreply.github.com>
2022-08-05 13:51:12 +0300

    Update SpringBootConfig.java

--- ‏‏קישור למסד נתונים - עותק/src/main/java/com/javatpoint/SpringBootConfig.java
+++ ‏‏קישור למסד נתונים - עותק/src/main/java/com/javatpoint/SpringBootConfig.java
@@ -2,8 +2,7 @@ package com.javatpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-//מכאן מתחילים להריץ את האפליקציה
-//מסד הנתונים נטען לבד
+
 @SpringBootApplication
 public class SpringBootConfig
 {
